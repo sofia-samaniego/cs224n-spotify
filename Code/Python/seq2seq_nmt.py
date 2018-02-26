6b551ae081bbf4c856e06359b9c174a1886e5f62
Sofía Samaniego de la Fuente <sofiasf@Sofias-MacBook-Pro.local>
2018-02-26 12:34:01 -0800

    Working model w/inference (need to fix rouge)

--- Code/Python/seq2seq_nmt.py
+++ Code/Python/seq2seq_nmt.py
@@ -1,10 +1,3 @@
-"""
-This script converts the tracklists of the playlists into sequences
-of word indices into the embedding matrix from the 100-dimensional
-pre-trained GloVe vectors found here:
-    http://nlp.stanford.edu/projects/glove/
-"""
-
 import re
 import sys
 import tensorflow as tf
@@ -12,10 +5,11 @@ import numpy as np
 import time
 import os
 
-from model import Model
+from model_nmt import Model
 from data_util import load_and_preprocess_data, UNK_TOKEN, START_TOKEN, END_TOKEN, PAD_TOKEN
 from util import Progbar, minibatches
 from rouge import rouge_n
+from tensorflow.python.layers import core as layers_core
 
 import logging
 from datetime import datetime
@@ -47,21 +41,28 @@ class Config:
     decoder_hidden_units = 20
 
 def padded_batch(batch, max_length, voc = None, option = None):
-    padded_batch = []
+    padded_items, mask_sequences = [], []
     for item in batch:
+        original_len = len(item)
         if len(item) >= max_length:
             padded_item = item[:max_length]
+            mask_sequence = [True]*max_length
         else:
             padded_item = item + [voc[PAD_TOKEN]]*(max_length - len(item))
+            mask_sequence = [True]*len(item) + [False]*(max_length - len(item))
         if option == 'decoder_inputs':
             padded_item = padded_item[0:-1]
+            mask_sequence = mask_sequence[0:-1]
             padded_item.insert(0, voc[START_TOKEN])
+            mask_sequence.insert(0, True)
         if option == 'decoder_targets':
             padded_item = padded_item[0:-1]
-            padded_item += [voc[END_TOKEN]]
-        padded_batch.append(padded_item)
-    return padded_batch
-
+            mask_sequence = mask_sequence[0:-1]
+            padded_item.insert(original_len, voc[END_TOKEN])
+            mask_sequence.insert(original_len, True)
+        padded_items.append(padded_item)
+        mask_sequences.append(mask_sequence)
+    return (padded_items, mask_sequences)
 
 class SequencePredictor(Model):
 
@@ -70,42 +71,45 @@ class SequencePredictor(Model):
         Generates placeholder variables to represent the input and target tensors
         """
         self.encoder_inputs = tf.placeholder(tf.int32,  shape = (None, self.config.max_length_x),
-                                                          name  = "encoder_inputs")
+                                                        name  = "encoder_inputs")
         self.decoder_targets = tf.placeholder(tf.int32, shape =(None, self.config.max_length_y),
-                                                          name  = "decoder_targets")
-        self.decoder_inputs = tf.placeholder(tf.int32, shape=(None, self.config.max_length_y),
-                                                         name = "decoder_inputs")
+                                                        name  = "decoder_targets")
+        self.decoder_inputs  = tf.placeholder(tf.int32, shape=(None, self.config.max_length_y),
+                                                        name = "decoder_inputs")
 
-    def create_feed_dict(self, inputs_batch, targets_batch = None):
+        self.length_encoder_inputs = tf.placeholder(tf.int32, shape = (None), name = "length_encoder_inputs")
+        self.length_decoder_inputs = tf.placeholder(tf.int32, shape = (None), name = "length_decoder_inputs")
+
+    def create_feed_dict(self, inputs_batch, length_encoder_batch, length_decoder_batch = None,
+                                                                   decoder_inputs_batch = None,
+                                                                   targets_batch = None):
         """
         Creates the feed_dict for the model.
         """
-        encoder_inputs_padded = padded_batch(inputs_batch,
-                                             self.config.max_length_x,
-                                             self.config.voc)
 
         if targets_batch is not None:
-            decoder_inputs_padded = padded_batch(targets_batch,
-                                                 self.config.max_length_y,
-                                                 self.config.voc,
-                                                 option = 'decoder_inputs')
-            decoder_targets_padded = padded_batch(targets_batch,
-                                                  self.config.max_length_y,
-                                                  self.config.voc,
-                                                  option = 'decoder_targets')
             feed_dict = {
-                            self.encoder_inputs: encoder_inputs_padded,
-                            self.decoder_inputs: decoder_inputs_padded,
-                            self.decoder_targets: decoder_targets_padded
+                            self.encoder_inputs: inputs_batch,
+                            self.decoder_inputs: decoder_inputs_batch,
+                            self.decoder_targets: targets_batch,
+                            self.length_encoder_inputs: length_encoder_batch,
+                            self.length_decoder_inputs: length_decoder_batch,
                         }
         else:
             feed_dict = {
-                            self.encoder_inputs: encoder_inputs_padded
+                            self.encoder_inputs: inputs_batch,
+                            self.length_encoder_inputs: length_encoder_batch,
                         }
 
         return feed_dict
 
-    def add_embeddings(self):
+    def get_embedding(self, scope):
+        with tf.variable_scope(scope, reuse=tf.AUTO_REUSE) as scope:
+            E = tf.get_variable("E", initializer = self.pretrained_embeddings)
+            return E
+
+    def add_embeddings(self, scope):
+    #def add_embeddings(self):
         """Adds an embedding layer that maps from input tokens (integers) to vectors and then
         concatenates those vectors:
             - Creates a tf.Variable and initializes it with self.pretrained_embeddings.
@@ -116,11 +120,13 @@ class SequencePredictor(Model):
             encoder_inputs_embedded: tf.Tensor of shape (None, embed_size)
             decoder_inputs_embedded: tf.Tensor of shape (None, embed_size)
         """
-        E = tf.get_variable("E", initializer = self.pretrained_embeddings)
-        encoder_inputs_embedded = tf.nn.embedding_lookup(E, self.encoder_inputs)
-        decoder_inputs_embedded = tf.nn.embedding_lookup(E, self.decoder_inputs)
+        with tf.variable_scope(scope, reuse=tf.AUTO_REUSE) as scope:
+            E = tf.get_variable("E", initializer = self.pretrained_embeddings)
+            #E = self.get_embedding("embedding_scope")
+            encoder_inputs_embedded = tf.nn.embedding_lookup(E, self.encoder_inputs)
+            decoder_inputs_embedded = tf.nn.embedding_lookup(E, self.decoder_inputs)
 
-        return encoder_inputs_embedded, decoder_inputs_embedded
+            return encoder_inputs_embedded, decoder_inputs_embedded, E
 
     def add_prediction_op(self):
         """Runs an rnn on the input using TensorFlows's
@@ -137,24 +143,43 @@ class SequencePredictor(Model):
         """
 
         # Encoder
-        encoder_cell = tf.contrib.rnn.LSTMCell(self.config.encoder_hidden_units)
-        encoder_inputs_embedded, decoder_inputs_embedded = self.add_embeddings()
-        _, encoder_final_state = tf.nn.dynamic_rnn(encoder_cell, encoder_inputs_embedded, dtype = tf.float32)
+        encoder_cell = tf.contrib.rnn.BasicLSTMCell(self.config.encoder_hidden_units)
+        encoder_inputs_embedded, decoder_inputs_embedded, E = self.add_embeddings("emb_scope")
+        _, encoder_final_state = tf.nn.dynamic_rnn(encoder_cell,
+                                                   encoder_inputs_embedded,
+                                                   sequence_length = self.length_encoder_inputs,
+                                                   dtype = tf.float32)
+
+        # Helpers for train and inference
+        self.length_decoder_inputs.set_shape([None])
+        train_helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs_embedded,
+                                                         self.length_decoder_inputs)
+
+        start_tokens = tf.fill([self.config.batch_size], self.config.voc[START_TOKEN])
+        end_token = self.config.voc[END_TOKEN]
+        pred_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(E, start_tokens, end_token)
 
         # Decoder
-        decoder_cell = tf.contrib.rnn.LSTMCell(self.config.decoder_hidden_units)
-        decoder_outputs, decoder_state = tf.nn.dynamic_rnn(decoder_cell,
-                                                           decoder_inputs_embedded,
-                                                           initial_state = encoder_final_state,
-                                                           dtype = tf.float32,
-                                                           scope = "plain_decoder")
+        def decode(helper, scope, reuse = None):
+            # Here could add attn_cell, etc. (see https://gist.github.com/ilblackdragon/)
+            decoder_cell = tf.contrib.rnn.BasicLSTMCell(self.config.decoder_hidden_units)
+            projection_layer = layers_core.Dense(self.config.voc_size, use_bias = False)
+            maximum_iterations = self.config.max_length_y
+            decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell,
+                                                      helper,
+                                                      encoder_final_state,
+                                                      output_layer = projection_layer)
+            outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder,
+                                                maximum_iterations = maximum_iterations,
+                                                impute_finished = True)
+            return outputs.rnn_output
 
-        preds = tf.contrib.layers.linear(decoder_outputs, self.config.voc_size)
-        #decoder_prediction = tf.argmax(preds, 2)
+        train_outputs = decode(train_helper, 'decode')
+        pred_outputs = tf.argmax(decode(pred_helper, 'decode'),2)
 
-        return preds
+        return train_outputs, pred_outputs
 
-    def add_loss_op(self, preds):
+    def add_loss_op(self, train_preds):
         """
         Adds ops to compute the stepwise cross-entropy loss function.
         Args:
@@ -164,9 +189,12 @@ class SequencePredictor(Model):
             loss: A 0-d tensor (scalar)
         """
         y = self.decoder_targets
-        stepwise_cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y,
-                                                                                logits = preds)
-        loss = tf.reduce_mean(stepwise_cross_entropy)
+        cross_ent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y,
+                                                                   logits = train_preds)
+        target_weights = tf.sequence_mask(self.length_decoder_inputs,
+                                          self.config.max_length_y,
+                                          dtype = train_preds.dtype)
+        loss = tf.reduce_sum(cross_ent*target_weights)/tf.to_float(self.config.batch_size)
 
         return loss
 
@@ -197,11 +225,42 @@ class SequencePredictor(Model):
         """Perform one step of gradient descent on the provided batch of data.
         This version also returns the norm of gradients.
         """
-        feed = self.create_feed_dict(inputs_batch, targets_batch=targets_batch)
+        inputs_batch_padded, _ = padded_batch(inputs_batch, self.config.max_length_x, self.config.voc)
+        length_inputs_batch = np.asarray([min(config.max_length_x,len(item)) for item in inputs_batch])
+
+        if targets_batch is None:
+            feed = self.create_feed_dict(inputs_batch_padded, length_inputs_batch)
+        else:
+            decoder_batch_padded, _ = padded_batch(targets_batch, self.config.max_length_y,
+                                                   self.config.voc, option = 'decoder_inputs')
+            targets_batch_padded, _ = padded_batch(targets_batch, self.config.max_length_y,
+                                                   self.config.voc, option = 'decoder_targets')
+            # length_targets_batch = np.asarray([min(config.max_length_y, len(item)+1) for item in targets_batch])
+            length_targets_batch = np.asarray([config.max_length_y for item in targets_batch])
+            feed = self.create_feed_dict(inputs_batch_padded,
+                                         length_inputs_batch,
+                                         length_targets_batch,
+                                         decoder_batch_padded,
+                                         targets_batch_padded)
         _, loss, grad_norm = sess.run([self.train_op, self.loss, self.grad_norm], feed_dict=feed)
         return loss, grad_norm
 
-    def run_epoch(self, sess, train):
+    def predict_on_batch(self, sess, inputs_batch):
+        """Make predictions for the provided batch of data
+
+        Args:
+            sess: tf.Session()
+            input_batch: np.ndarray of shape (n_samples, n_features)
+        Returns:
+            predictions: np.ndarray of shape (n_samples, n_classes)
+        """
+        inputs_batch_padded, _ = padded_batch(inputs_batch, self.config.max_length_x, self.config.voc)
+        length_inputs_batch = np.asarray([min(config.max_length_x,len(item)) for item in inputs_batch])
+        feed = self.create_feed_dict(inputs_batch_padded, length_inputs_batch)
+        predictions = sess.run(self.infer_pred, feed_dict=feed)
+        return predictions
+
+    def run_epoch(self, sess, saver, train, dev):
         prog = Progbar(target=1 + int(len(train) / self.config.batch_size))
         losses, grad_norms = [], []
         for i, batch in enumerate(minibatches(train, self.config.batch_size)):
@@ -210,30 +269,38 @@ class SequencePredictor(Model):
             grad_norms.append(grad_norm)
             prog.update(i + 1, [("train loss", loss)])
 
-        return losses, grad_norms
+        print("\nEvaluating on dev set...")
+        predictions = []
+        references = []
+        for batch in minibatches(dev, self.config.batch_size):
+            inputs_batch, targets_batch = batch
+            targets_padded, _ = padded_batch(targets_batch, self.config.max_length_y, self.config.voc, option = 'targets_batch')
+            prediction = list(self.predict_on_batch(sess, inputs_batch))
+            print(prediction[0])
+            print(targets_padded[0])
+            predictions.append(prediction)
+            references.append(targets_padded)
 
-    def fit(self, sess, train):
-    #def fit(self, sess, saver, train, dev):
-        losses, grad_norms = [], []
+        f1, _, _ = rouge_n(predictions, references)
+        print(f1)
+        print("- dev rouge f1: {}".format(f1 * 100.0))
+        return losses, grad_norms, predictions, f1
+
+    def fit(self, sess, saver, train, dev):
+        losses, grad_norms, predictions = [], [], []
+        best_dev_ROUGE = 0.0
         for epoch in range(self.config.n_epochs):
             logger.info("Epoch %d out of %d", epoch + 1, self.config.n_epochs)
-            loss, grad_norm = self.run_epoch(sess, train)
+            loss, grad_norm, preds, f1 = self.run_epoch(sess, saver, train, dev)
+            if f1 > best_dev_ROUGE:
+                best_dev_ROUGE = f1
+                if saver:
+                    print("New best dev ROUGE! Saving model in ./data/weights/model.weights")
+                    saver.save(sess, './data/weights/model.weights')
             losses.append(loss)
             grad_norms.append(grad_norm)
-
-        return losses, grad_norms
-
-    # def fit(self, sess, saver, parser, train_examples, dev_set):
-        # best_dev_UAS = 0
-        # for epoch in range(self.config.n_epochs):
-            # print "Epoch {:} out of {:}".format(epoch + 1, self.config.n_epochs)
-            # dev_UAS = self.run_epoch(sess, parser, train_examples, dev_set)
-            # if dev_UAS > best_dev_UAS:
-                # best_dev_UAS = dev_UAS
-                # if saver:
-                    # print "New best dev UAS! Saving model in ./data/weights/parser.weights"
-                    # saver.save(sess, './data/weights/parser.weights')
-            # print
+            predictions.append(preds)
+        return losses, grad_norms, predictions
 
     def __init__(self, config, pretrained_embeddings):
         self.pretrained_embeddings = pretrained_embeddings
@@ -241,6 +308,8 @@ class SequencePredictor(Model):
         self.encoder_inputs = None
         self.decoder_inputs = None
         self.decoder_targets = None
+        self.length_encoder_inputs = None
+        self.length_decoder_inputs = None
         self.grad_norm = None
         self.build()
 
@@ -278,8 +347,9 @@ if __name__ == '__main__':
         print("Building model...")
         model = SequencePredictor(config, E)
         init = tf.global_variables_initializer()
-        #saver = tf.train.Saver()
+        saver = tf.train.Saver()
         print("Took {} seconds to build model".format(time.time() - start))
+
     graph.finalize()
 
     with tf.Session(graph = graph) as sess:
@@ -287,32 +357,19 @@ if __name__ == '__main__':
         print(80 * "=")
         print("TRAINING")
         print(80 * "=")
-        losses, grad_norms = model.fit(sess, train)
-
-# def main(debug=True):
-
-    # with tf.Session(graph=graph) as session:
-        # parser.session = session
-        # session.run(init_op)
-
-        # print 80 * "="
-        # print "TRAINING"
-        # print 80 * "="
-        # model.fit(session, saver, parser, train_examples, dev_set)
-
-        # if not debug:
-            # print 80 * "="
-            # print "TESTING"
-            # print 80 * "="
-            # print "Restoring the best model weights found on the dev set"
-            # saver.restore(session, './data/weights/parser.weights')
-            # print "Final evaluation on test set",
-            # UAS, dependencies = parser.parse(test_set)
-            # print "- test UAS: {:.2f}".format(UAS * 100.0)
-            # print "Writing predictions"
-            # with open('q2_test.predicted.pkl', 'w') as f:
-                # cPickle.dump(dependencies, f, -1)
-            # print "Done!"
-
-
+        losses, grad_norms, predictions = model.fit(sess, saver, test, test)
+        #losses, grad_norms, predictions = model.fit(sess, saver, train, dev)
+        if not debug:
+            print(80 * "=")
+            print("TESTING")
+            print(80 * "=")
+            print("Restoring the best model weights found on the dev set")
+            saver.restore(session, './data/weights/model.weights')
+            print("Final evaluation on test set")
+            #UAS, dependencies = parser.parse(test_set)
+            #print("- test UAS: {:.2f}".format(UAS * 100.0))
+            #print("Writing predictions")
+            #with open('q2_test.predicted.pkl', 'w') as f:
+            #    cPickle.dump(dependencies, f, -1)
+            print("Done!")
 
